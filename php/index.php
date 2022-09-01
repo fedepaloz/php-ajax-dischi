d26e140ea74f04382531bf45e387d604dfa42b33
fedepaloz <palozzifederico@gmail.com>
2022-09-01 16:14:44 +0200

    style dischi, folder discs php.

---
+++ php/index.php
@@ -0,0 +1,44 @@
+<?php 
+include __DIR__ . '/./discs.php';
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../style.css">
+
+
+</head>
+
+<body>
+
+    <header>
+
+    </header>
+
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-2 text-white">
+                <img class='img-fluid' src="https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg" alt="">
+                <h2>titolo</h2>
+                <h3>autore</h3>
+                <h4>genere</h4>
+            </div>
+
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
